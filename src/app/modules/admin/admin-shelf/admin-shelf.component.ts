e77b0a6a0a0077caeee9a48ba3084b721681e45e
nazlinoyantastan <nazlinoyan_88@hotmail.com>
2024-04-22 12:33:38 +0300

    admin-add-shelf

---
+++ src/app/modules/admin/admin-shelf/admin-shelf.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-shelf',
+  templateUrl: './admin-shelf.component.html',
+  styleUrl: './admin-shelf.component.scss'
+})
+export class AdminShelfComponent {
+
+}
