e1095776e255192a990805671e3cc2efc6a75aed
Emir <guleremir24@gmail.com>
2024-04-23 17:10:29 +0300

    update

--- src/app/core/component/admin-panel/admin-panel.component.ts
+++ src/app/core/component/admin-panel/admin-panel.component.ts
@@ -2,18 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { LoginService } from '../../service/login.service';
+import { Product } from '../../../shared/dto/product';
+import { ProductService } from '../../../shared/service/product.service';
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
   styleUrl: './admin-panel.component.scss'
 })
-export class AdminPanelComponent implements OnInit{
+export class AdminPanelComponent implements OnInit {
   url = "";
+  products: Product[] = []; 
   constructor(
     private router: Router,
     private toastr: ToastrService,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private productService: ProductService
   ) { }
   ngOnInit(): void {
     this.router.events.subscribe({
@@ -23,8 +27,25 @@ export class AdminPanelComponent implements OnInit{
         }
       }
     });
+    this.productService.getAllProduct().subscribe({
+      next: (products => {
+        console.log(products);
+        this.products = products;
+      })
+    });
   }
-
+//   calculateProgress(): number {
+//     if (this.products.length > 0) {
+//         // Tüm ürünlerin sayısını 200'e göre oranlayarak ilerleme yüzdesini hesaplar
+//         return (this.products.length / 60) * 100;
+//     } else {
+//         // Eğer ürün yoksa ilerleme çubuğu boş olmalı
+//         return 0;
+//     }
+// }
+calculateProgress(): number {
+  return (this.products.length / 70) * 100; // 500000, maksimum sayfa görünümü
+}
   logout() {
     this.loginService.logout();
     this.toastr.success("Logout Successfuly!");
@@ -33,6 +54,6 @@ export class AdminPanelComponent implements OnInit{
 
   isCurrentRoute(route: string): boolean {
     return this.router.url === route;
-}
+  }
 
 }
--- src/app/modules/admin/admin-edit-user/admin-edit-user.component.ts
+++ src/app/modules/admin/admin-edit-user/admin-edit-user.component.ts
@@ -61,11 +61,15 @@ export class AdminEditUserComponent  implements OnInit {
     let email = this.updateForm.get('email')!.value;
     let password = this.updateForm.get('password')!.value;
     let confirmPassword = this.updateForm.get('confirmPassword')!.value;
-    let roles = this.updateForm.get('roles')!.value; // Rollerin alınması
-
+    let selectedRoles: UserRole[] = this.selectedRoles.map(role => {
+      return {
+        id: role.id,
+        name: role.name // veya role.name gibi
+      };
+    });
     
       if (password === confirmPassword) {
-        this.userService.updateUser(new User(this.userID, email, password, roles)).subscribe({
+        this.userService.updateUser(new User(this.userID, email, password, selectedRoles)).subscribe({
           next: (result) => {
             this.toastr.info('User updated.');
             this.router.navigate(['..'], { relativeTo: this.route });
